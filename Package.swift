a60ab40b9ec595885b69739a665ae284676a7d82
Andrey Fedorov <m3g0byt3@gmail.com>
2024-10-10 16:10:02 +0200

    IOS-7988: Update `Package.swift` to use with swift-tools-version 5.10; Replace some SPM deps with reusable ones

--- Package.swift
+++ Package.swift
@@ -1,41 +1,39 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
     name: "BinanceChain",
     platforms: [
-        .macOS(.v10_11),
-        .iOS(.v11),
-        .tvOS(.v11),
-        .watchOS(.v2)
+        .macOS(.v12),
+        .iOS(.v15),
     ],
     products: [
-        .library(name: "BinanceChain", targets: ["binancechain"]),
+        .library(name: "BinanceChain", targets: ["binancechain"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.4.0"),
+        .package(url: "https://github.com/tangem/swift-protobuf-binaries.git", exact: "1.25.2-tangem1"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.8.2"),
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.0"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.3.0"),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
         .package(url: "https://github.com/malcommac/SwiftDate", from: "6.0.1"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.0.0"),
-        .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.4"),
+        .package(url: "https://github.com/GigaBitcoin/secp256k1.swift.git", .upToNextMinor(from: "0.12.0")),
     ],
     targets: [
         .target(
             name: "binancechain",
             dependencies: [
-                "SwiftProtobuf",
+                .product(name: "SwiftProtobuf", package: "swift-protobuf-binaries"),
+                .product(name: "secp256k1", package: "secp256k1.swift"),
                 "Alamofire",
                 "SwiftyJSON",
                 "Starscream",
-                "XCGLogger", 
-                "secp256k1",
+                "XCGLogger",
                 "SwiftDate",
                 "CryptoSwift"
             ],
             path: "BinanceChain/Sources"
-        )
+        ),
     ]
 )
