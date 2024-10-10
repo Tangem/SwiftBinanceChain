9d8207d1358b18588d4dcd7855b9609fa976369c
Andrey Fedorov <m3g0byt3@gmail.com>
2024-10-10 17:53:42 +0200

    IOS-7988: Add missing SPM dep

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
         .library(name: "BinanceChain", targets: ["binancechain"])
     ],
     dependencies: [
+        .package(url: "https://github.com/tangem/swift-protobuf-binaries.git", exact: "1.25.2-tangem1"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.8.2"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.3.0"),
         .package(url: "https://github.com/malcommac/SwiftDate", from: "6.0.1"),
@@ -19,6 +20,7 @@ let package = Package(
         .target(
             name: "binancechain",
             dependencies: [
+                .product(name: "SwiftProtobuf", package: "swift-protobuf-binaries"),
                 "Alamofire",
                 "SwiftyJSON",
                 "SwiftDate",
