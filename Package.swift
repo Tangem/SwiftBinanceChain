c17bea4cccfa784e22821a196abc99312ac15906
Andrey Fedorov <m3g0byt3@gmail.com>
2024-10-12 00:54:25 +0200

    IOS-7988: Got SPM setup up and running

--- Package.swift
+++ Package.swift
@@ -8,17 +8,17 @@ let package = Package(
         .iOS(.v15),
     ],
     products: [
-        .library(name: "BinanceChain", targets: ["binancechain"])
+        .library(name: "BinanceChain", targets: ["BinanceChain"])
     ],
     dependencies: [
         .package(url: "https://github.com/tangem/swift-protobuf-binaries.git", exact: "1.25.2-tangem1"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.8.2"),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.3.0"),
-        .package(url: "https://github.com/malcommac/SwiftDate", from: "6.0.1"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
+        .package(url: "https://github.com/malcommac/SwiftDate", from: "7.0.0"),
     ],
     targets: [
         .target(
-            name: "binancechain",
+            name: "BinanceChain",
             dependencies: [
                 .product(name: "SwiftProtobuf", package: "swift-protobuf-binaries"),
                 "Alamofire",
